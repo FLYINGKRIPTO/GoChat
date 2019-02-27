d7571fa0e6489a98d16a8d590d96634599a0f7f3
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2019-02-27 08:03:19 +0530

    profile picture uploading done

--- app/src/main/java/com/example/gochat/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/gochat/Fragments/ProfileFragment.java
@@ -1,16 +1,27 @@
 package com.example.gochat.Fragments;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.gochat.Model.User;
 import com.example.gochat.R;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -18,8 +29,16 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class ProfileFragment extends Fragment {
@@ -29,6 +48,14 @@ public class ProfileFragment extends Fragment {
 
     FirebaseUser firebaseUser;
     DatabaseReference reference;
+
+    //to access storage
+    StorageReference storageReference;
+    private static final int IMAGE_STORAGE = 1;
+    private Uri imageUri;
+    private StorageTask uploadTask;
+    private static final String TAG = "ProfileFragment";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -37,6 +64,8 @@ public class ProfileFragment extends Fragment {
 
         image_profile = view.findViewById(R.id.profile_image_pr);
         username = view.findViewById(R.id.username_pr);
+
+        storageReference= FirebaseStorage.getInstance().getReference("uploads");
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
@@ -51,6 +80,7 @@ public class ProfileFragment extends Fragment {
 
                 }
                 else {
+                    Log.d(TAG, "onDataChange: image url "+ user.getImageURL());
                     Glide.with(getContext()).load(user.getImageURL()).into(image_profile);
                 }
             }
@@ -60,8 +90,95 @@ public class ProfileFragment extends Fragment {
 
             }
         });
+        image_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImage();
+            }
+        });
         return view;
     }
 
+    private void openImage() {
 
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,IMAGE_STORAGE);
+    }
+    private String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContext().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+
+    }
+    private void uploadImage(){
+        final ProgressDialog pd = new ProgressDialog(getContext());
+        pd.setMessage("Uploading");
+        pd.show();
+
+        if(imageUri != null){
+            final StorageReference fileReference  =storageReference.child(System.currentTimeMillis()
+            +"."+getFileExtension(imageUri));
+
+            uploadTask = fileReference.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot,Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot>task) throws Exception {
+                    if (!task.isSuccessful()){
+                        throw  task.getException();
+                    }
+
+                    return fileReference.getDownloadUrl();
+                }
+
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()){
+                            Uri downloadUri = task.getResult();
+                            String mUri = downloadUri.toString();
+
+                            reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+                            HashMap<String, Object> map = new HashMap<>();
+                            map.put("imageURL",mUri);
+                            Log.d(TAG, "onComplete: imageUri "+ mUri);
+                            reference.updateChildren(map);
+                            pd.dismiss();
+                        }
+                        else {
+                            Toast.makeText(getContext(),"Failed",Toast.LENGTH_SHORT).show();
+                        }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+                    pd.dismiss();
+                }
+            });
+
+        }
+        else {
+            Toast.makeText(getContext(),"NO image Selected",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == IMAGE_STORAGE && resultCode == RESULT_OK
+                && data != null && data.getData() != null
+        ){
+            imageUri= data.getData();
+            if (uploadTask != null && uploadTask.isInProgress()){
+                Toast.makeText(getContext(),"Upload in Progress ",Toast.LENGTH_SHORT).show();
+            }
+            else {
+                uploadImage();
+            }
+        }
+    }
 }
